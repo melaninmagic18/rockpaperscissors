c719c1aed2c00dac585fe0b32f1f00a9d375e37d
Rea Gibson <gibsonrea18@gmail.com>
2018-11-14 23:14:46 +0000

    math random

--- js/script.js
+++ js/script.js
@@ -10,5 +10,27 @@
 // DOCUMENT READY FUNCTION BELOw
 
 $('#shoot').click(function() {
-   $('#userChoice').text($('#input').val()); 
+    
+       
+    userChoice = $('#input').val();
+    $('#userChoice').text(userChoice);
+    var randomNumber = Math.random();
+    
+    console.log(randomNumber);
+    
+    if (randomNumber >.66666) {
+        computerChoice = "rock";
+    }
+    else if (randomNumber >.33333 && randomNumber <.66666) {
+         computerChoice = "paper";
+        
+    }
+    else if (randomNumber <.33333){
+        computerChoice = "scissors";
+    }
 });
+ 
+ var userChoice = "";
+ var computerChoice = "";
+ var winner = "";
+ 
